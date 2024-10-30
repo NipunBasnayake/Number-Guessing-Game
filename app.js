8648f6c49b75766822faec706ad41632efa8f259
Nipun Sathsara Basnayake <nipunsathsara1999@gmail.com>
2024-10-30 15:26:05 +0530

    Alert Commited

--- app.js
+++ app.js
@@ -11,22 +11,22 @@ function guess() {
         let txtInput = Number(document.getElementById("txtInput").value);
         if (txtInput === randomNum) {
             lblResult.innerHTML = `<p class="text-success">You win.!🥳 The Correct number was ${randomNum}</p>`;
-            alert("ඔයා හරි අනේ...🥳");
+            // alert("ඔයා හරි අනේ...🥳");
             lblChances.innerHTML = "";
         } else {
             if(randomNum>txtInput){
                 lblResult.innerHTML = `<p class="text-primary">Number is low.🔽 Try guessing a higher number.!</p>`;
-                alert("පොඩි වැඩී...😏 ලොකු Number එකක් දාන්න...");
+                // alert("පොඩි වැඩී...😏 ලොකු Number එකක් දාන්න...");
             }else{
                 lblResult.innerHTML = `<p class="text-primary">Number is high.🔼 Try guessing a lower number.!</p>`;
-                alert("ලොකු වැඩී...🤯 පොඩි Number එකක් දාන්න...");
+                // alert("ලොකු වැඩී...🤯 පොඩි Number එකක් දාන්න...");
             }
             count--;
             lblChances.innerHTML = `Chances left: ${count}`;
         }
     } else {
         lblResult.innerHTML = `<p class="text-danger">Game Over! No more chances</p>`;
-        alert("චාන්ස් ඉවරයි, ආයෙ දෙන්නෑ...😒");
+        // alert("චාන්ස් ඉවරයි, ආයෙ දෙන්නෑ...😒");
         // imgDiv.innerHTML = `<div id="imgDiv" class"text-center"><img src="https://media.tenor.com/DMWqIb2Rdp4AAAAi/bonk-cheems.gif" alt=""></div>`;
     }
 }
